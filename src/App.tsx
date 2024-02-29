a590c9448a2f9b2e225942c08b19933ada1b5832
arthur andrade <arthurandrad.23@gmail.com>
2024-02-28 21:33:40 -0300

    App finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
-function App() {
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export default function App() {
+  const [error,setError] = useState();
+  const [isLoading,setIsLoading] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [page,setPage] = useState(0);
+
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  useEffect (() => {
+    const fetchPosts = async () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController();
+
+      setIsLoading(true);
+
+      try{
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`,{
+          signal: abortControllerRef.current?.signal,
+        });
+        const posts = (await response.json()) as Post[]
+        setPosts(posts);
+      } catch (e:any) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, [page])
+
+  if (error) {
+    return <div>Something went wrong! Please try again.</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Fetching in React</h1>
+      <button onClick={() => setPage(page + 1)}>Increse page request ({page})</button>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading &&  (
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>
+          })}
+        </ul>
+      )}
     </div>
   );
 }
-
-export default App;
